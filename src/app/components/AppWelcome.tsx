9fc2d3c23cbbf55616de4ac85896673b28756493
Paphadet <paphadet.tag123@gmailcom>
2025-05-27 11:59:33 +0700

    how to use prop

--- src/app/components/AppWelcome.tsx
+++ src/app/components/AppWelcome.tsx
@@ -1,7 +1,28 @@
-export default function AppWelcome() {
-    return (
-        <>
-            <p>welcome tot cosci</p>
-        </>
-    )
+"use client"
+
+type AppWelcomeProps = {
+    headTitle: string
+    isShow: boolean
+}
+export default function AppWelcome({headTitle , isShow} : AppWelcomeProps) {
+  const title = 'Welcome to cosci';
+  const currentYear = <p>2025</p>;
+  const handleClick = () => {
+    alert('Hello TypeScript');
+  }
+
+  return (
+    <>
+      <h1>{headTitle}</h1>
+      <p>{title.toUpperCase()}</p>
+      <button className="bg-blue-700 p-3 m-3 text-white rounded-lg" onClick={handleClick}>กดครับน้อง!</button>
+      {currentYear}
+      {
+      isShow && <p>Date: 10/10/1988</p>
+      }
+      {
+        isShow ? <p>Hello Next.js</p> : <p>Hello JavaScript</p>
+      }
+    </>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,10 @@
 import AppWelcome from "./components/AppWelcome";
 
 export default function Home() {
-  const title = 'Welcome to cosci';
-  const currentYear = <p>2025</p>;
-  const isShow = true;
-
   return (
     <div>
       <h1>Homepage</h1>
-      <AppWelcome />
-      <p>{title.toUpperCase()}</p>
-      {currentYear}
-      {
-      isShow && <p>Date: 10/10/1988</p>
-      }
-      {
-        isShow ? <p>Hello Next.js</p> : <p>Hello JavaScript</p>
-      }
+      <AppWelcome headTitle="Big O" isShow={true} />
     </div>
   );
 }
