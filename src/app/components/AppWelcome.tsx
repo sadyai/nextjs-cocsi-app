39b17e73cef399e041747edb18a5a97109b38d73
Paphadet <paphadet.tag123@gmailcom>
2025-05-27 15:07:35 +0700

    env

--- src/app/components/AppWelcome.tsx
+++ src/app/components/AppWelcome.tsx
@@ -1,21 +1,29 @@
 "use client"
 
+import { Button } from "@/components/ui/button"
+import { useState } from "react"
+
 type AppWelcomeProps = {
     headTitle: string
     isShow: boolean
 }
 export default function AppWelcome({headTitle , isShow} : AppWelcomeProps) {
-  const title = 'Welcome to cosci';
+  //let title = 'Welcome to cosci';
+  const [title,setTitle] = useState('Welcome to cosci')
   const currentYear = <p>2025</p>;
   const handleClick = () => {
+  //title = 'Welcome to SWU'
+    setTitle('Welcome to SWU')
     alert('Hello TypeScript');
   }
 
   return (
     <>
       <h1>{headTitle}</h1>
+      <p>{process.env.NEXT_PUBLIC_APP_NAME}</p>
       <p>{title.toUpperCase()}</p>
-      <button className="bg-blue-700 p-3 m-3 text-white rounded-lg" onClick={handleClick}>กดครับน้อง!</button>
+      <Button onClick={handleClick}>Click Me</Button>
+      <button className="bg-blue-700 p-3 m-3 text-black rounded-lg" onClick={handleClick}>กดครับน้อง!</button>
       {currentYear}
       {
       isShow && <p>Date: 10/10/1988</p>
