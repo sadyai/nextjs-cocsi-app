bf3dddb7c469b08c7e2628eae7bc7ca1d9f74ff5
Paphadet <paphadet.tag123@gmailcom>
2025-05-28 14:13:31 +0700

    apiInfo

---
+++ src/app/(back)/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { K2D } from "next/font/google";
+import "../globals.css";
+
+const k2D = K2D({
+   subsets: ['thai'],
+   weight: ['100','200', '300', '400', '500', '600', '700', '800'],
+   display: 'swap',
+});
+
+export const metadata: Metadata = {
+  title: "เข้าสู่ระบบ",
+  description: "ขายสินค้าของนักศึกษา ประจำปี 2568",
+};
+
+export default function backLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${k2D.className}`}
+      >
+
+        {children}
+      </body>
+    </html>
+  );
+}
--- src/app/(front)/about/page.tsx
+++ src/app/(front)/about/page.tsx
@@ -1,9 +1,15 @@
 import Contact01Page from "@/components/contact-01/contact-01";
 
-export default function About() {
+export default async function About() {
+    const data = await fetch("https://api.codingthailand.com/api/version");
+    const apiInfo = await data.json();
     return (
         <>
-            <Contact01Page/>
+            <p>{JSON.stringify(apiInfo)}</p>
+            {
+                apiInfo&&<p>{apiInfo.data.version}</p>
+            }
+            <Contact01Page version={apiInfo.data.version}/>
         </>
     )
 }
