8356378f3c958459738f865d7912472e71a2a38b
Paphadet <paphadet.tag123@gmailcom>
2025-05-28 09:54:39 +0700

    Nav Bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { K2D } from "next/font/google";
 import "./globals.css";
+import AppNavbar from "./components/AppNavbar";
 
 const k2D = K2D({
   subsets: ["thai"],
@@ -23,6 +24,8 @@ export default function RootLayout({
       <body
         className={`${k2D.className}`}
       >
+        <AppNavbar/>
+        <hr />
         {children}
       </body>
     </html>
