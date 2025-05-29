8a8ded8fb2fddbedd019851d0aaee20ca1c5d6e3
Paphadet <paphadet.tag123@gmailcom>
2025-05-29 15:37:16 +0700

    Total & Clear Cart

--- src/app/(front)/components/CartList.tsx
+++ src/app/(front)/components/CartList.tsx
@@ -1,15 +1,24 @@
 'use client'
 
+import { Button } from "@/components/ui/button";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { useCartStore } from "@/lib/cart-store"
+import { Trash } from "lucide-react";
 
 export default function CartList() {
 
     const items = useCartStore((state) => state.items);
     const removeItem = useCartStore((state) => state.removeItem);
     const clearCart = useCartStore((state) => state.clearCart);
-    const totalPrice = useCartStore((state) => state.totalPrice);
+    const totalPrice = useCartStore((state) => state.totalPrice());
 
+    if (items.length === 0) {
+        return (
+           <div className="text-center mt-20">
+            สินค้าในตะกร้าสินค้าไม่มี กรุณาเพิ่มสินค้าก่อน
+           </div>
+        )
+    }
 
     return (
         <div className="max-auto max-w-4xl mt-20">
@@ -22,6 +31,7 @@ export default function CartList() {
                         <TableHead>ราคา</TableHead>
                         <TableHead>จำนวน</TableHead>
                         <TableHead>รวม</TableHead>
+                        <TableHead>เครื่องมือ</TableHead>
                     </TableRow>
                 </TableHeader>
                 <TableBody>
@@ -32,12 +42,26 @@ export default function CartList() {
                             <TableCell>{i.title}</TableCell>
                             <TableCell>{i.price}</TableCell>
                             <TableCell>{i.qty}</TableCell>
-                            <TableCell>{i.price * i.qty}</TableCell>
+                            <TableCell>{(i.price * i.qty).toFixed(2)}</TableCell>
+                            <TableCell>
+                                <Button variant="destructive" onClick={() => {removeItem(i.productId)}}>
+                                    <Trash />
+                                </Button>
+                            </TableCell>
                         </TableRow>
                     ))
                     }
                 </TableBody>
             </Table>
+            <div className="text-right mt-5">
+                    รวมทั้งหมด: {totalPrice.toFixed(2)} บาท
+            </div>
+            <div>
+                <Button onClick={() => clearCart()} className="mt-5">
+                    ล้างตะกร้าสินค้า
+                </Button>
+            </div>
         </div>
+        
     )
 }
