95fd64f735a639cf0ebf6b134e06548cfce120ae
Paphadet <paphadet.tag123@gmailcom>
2025-05-30 10:39:16 +0700

    connect database to Auth

--- auth-schema.ts
+++ auth-schema.ts
@@ -1,41 +1,31 @@
-import { mysqlTable,primaryKey, int, varchar, decimal, timestamp, boolean, text } from "drizzle-orm/mysql-core"
-import { sql } from "drizzle-orm"
-
-export const product = mysqlTable("product", {
-    id: int({ unsigned: true }).autoincrement().notNull(),
-    title: varchar({ length: 255 }),
-    price: decimal().notNull(),
-},
-(table) => [
-    primaryKey({ columns: [table.id], name: "product_id"}),
-]);
+import { mysqlTable, varchar, text, timestamp, boolean, int } from "drizzle-orm/mysql-core";
 
 export const user = mysqlTable("user", {
-                    id: varchar('id', { length: 36 }).primaryKey(),
-                    name: text('name').notNull(),
+					id: varchar('id', { length: 36 }).primaryKey(),
+					name: text('name').notNull(),
  email: varchar('email', { length: 255 }).notNull().unique(),
  emailVerified: boolean('email_verified').$defaultFn(() => false).notNull(),
  image: text('image'),
  createdAt: timestamp('created_at').$defaultFn(() => /* @__PURE__ */ new Date()).notNull(),
  updatedAt: timestamp('updated_at').$defaultFn(() => /* @__PURE__ */ new Date()).notNull()
-                });
+				});
 
 export const session = mysqlTable("session", {
-                    id: varchar('id', { length: 36 }).primaryKey(),
-                    expiresAt: timestamp('expires_at').notNull(),
+					id: varchar('id', { length: 36 }).primaryKey(),
+					expiresAt: timestamp('expires_at').notNull(),
  token: varchar('token', { length: 255 }).notNull().unique(),
  createdAt: timestamp('created_at').notNull(),
  updatedAt: timestamp('updated_at').notNull(),
  ipAddress: text('ip_address'),
  userAgent: text('user_agent'),
- userId: varchar('user_id',{length:36}).notNull().references(()=> user.id, { onDelete: 'cascade' })
-                });
+ userId: text('user_id').notNull().references(()=> user.id, { onDelete: 'cascade' })
+				});
 
 export const account = mysqlTable("account", {
-                    id: varchar('id', { length: 36 }).primaryKey(),
-                    accountId: text('account_id').notNull(),
+					id: varchar('id', { length: 36 }).primaryKey(),
+					accountId: text('account_id').notNull(),
  providerId: text('provider_id').notNull(),
- userId: varchar('user_id',{length:36}).notNull().references(()=> user.id, { onDelete: 'cascade' }),
+ userId: text('user_id').notNull().references(()=> user.id, { onDelete: 'cascade' }),
  accessToken: text('access_token'),
  refreshToken: text('refresh_token'),
  idToken: text('id_token'),
@@ -45,13 +35,13 @@ export const account = mysqlTable("account", {
  password: text('password'),
  createdAt: timestamp('created_at').notNull(),
  updatedAt: timestamp('updated_at').notNull()
-                });
+				});
 
 export const verification = mysqlTable("verification", {
-                    id: varchar('id', { length: 36 }).primaryKey(),
-                    identifier: text('identifier').notNull(),
+					id: varchar('id', { length: 36 }).primaryKey(),
+					identifier: text('identifier').notNull(),
  value: text('value').notNull(),
  expiresAt: timestamp('expires_at').notNull(),
  createdAt: timestamp('created_at').$defaultFn(() => /* @__PURE__ */ new Date()),
  updatedAt: timestamp('updated_at').$defaultFn(() => /* @__PURE__ */ new Date())
-                });
+				});
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,9 +1,19 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import db from "@/DB"; // your drizzle instance
+import { account, session, user, verification } from "@/DB/schema";
  
 export const auth = betterAuth({
     database: drizzleAdapter(db, {
         provider: "mysql", // or "mysql", "sqlite"
-    })
+        schema: {user,account,session,verification},
+        usePlural:false
+    }),
+    emailAndPassword: {
+        enabled: true,
+        autoSignIn: false,
+        requireEmailVerification: false,
+        minPasswordLength: 8,
+
+    }
 });
