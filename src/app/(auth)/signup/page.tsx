4d9143da145add3a859392bf1772fc5a1e4912e0
Paphadet <paphadet.tag123@gmailcom>
2025-05-30 10:00:17 +0700

    login-signup

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,7 +1,110 @@
-export default function Signup() {
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
+import { zodResolver } from "@hookform/resolvers/zod";
+import Link from "next/link";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const formSchema = z.object({
+  email: z.string().email("รูปแบบอีเมลไม่ถูกต้อง"),
+  password: z.string().min(8, "รหัสผ่อนไม่ควรน้อยกว่า 8 ตัวอักษร"),
+});
+
+const SignUP01Page = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+    resolver: zodResolver(formSchema),
+  });
+
+  const onSubmit = (data: z.infer<typeof formSchema>) => {
+    console.log(data);
+  };
+
   return (
-    <main>
-      <p>signup page</p>
-    </main>
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-xs w-full flex flex-col items-center">
+        
+        <p className="mt-4 text-xl font-bold tracking-tight">
+          Sign Up to Shadcn UI Blocks
+        </p>
+
+        <div className="my-7 w-full flex items-center justify-center overflow-hidden">
+          <Separator />
+        </div>
+
+        <Form {...form}>
+          <form
+            className="w-full space-y-4"
+            onSubmit={form.handleSubmit(onSubmit)}
+          >
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="email"
+                      placeholder="Email"
+                      className="w-full"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="password"
+                      placeholder="Password"
+                      className="w-full"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit" className="mt-4 w-full">
+              Continue with Email
+            </Button>
+          </form>
+        </Form>
+
+        <div className="mt-5 space-y-5">
+          <p className="text-sm text-center">
+            you already have an account?
+            <Link href="/login" className="ml-1 underline text-muted-foreground">
+              login
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
   );
-}
+};
+
+
+export default SignUP01Page;
